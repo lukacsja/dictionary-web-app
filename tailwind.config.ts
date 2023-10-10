ff93621c94f7755ef6ca8b0471b673a04d337476
Janos Lukacs <pszichae@gmail.com>
2023-10-10 18:12:28 +0200

    added color vars

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -13,8 +13,19 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'gray-8': 'var(--color-gray-8)',
+        'gray-7': 'var(--color-gray-7)',
+        'gray-6': 'var(--color-gray-6)',
+        'gray-5': 'var(--color-gray-5)',
+        'gray-4': 'var(--color-gray-4)',
+        'gray-3': 'var(--color-gray-3)',
+        'gray-2': 'var(--color-gray-2)',
+        'purple-main': 'var(--color-purple-main)',
+        'red-main': 'var(--color-red-main)',
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
