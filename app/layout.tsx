52802f5d9441fc0ed3881175c9576433226e1b47
Janos Lukacs <pszichae@gmail.com>
2023-11-27 20:25:21 +0100

    minor layout fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import { FontContextProvider } from '@/context/FontContext';
 import './globals.css';
 import type { Metadata } from 'next';
 import ThemeContextProvider from '@/context/ThemeContext';
-import Header from '@/components/header';
 
 export const metadata: Metadata = {
   title: 'dictionary web app',
@@ -17,13 +16,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body
-        className={`select-[#a445ed] flex min-h-screen w-full min-w-[240px] max-w-[737px] flex-col bg-white p-6 text-gray-6 selection:bg-[#a445ed] selection:text-white dark:bg-gray-8 dark:text-white md:px-10 md:py-14`}
+        className={`select-[#a445ed] flex min-h-screen justify-center bg-white text-gray-6 selection:bg-[#a445ed] selection:text-white dark:bg-gray-8 dark:text-white`}
       >
         <ThemeContextProvider>
-          <FontContextProvider>
-            <Header />
-            {children}
-          </FontContextProvider>
+          <FontContextProvider>{children}</FontContextProvider>
         </ThemeContextProvider>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Header from '@/components/header';
 import SearchField from '@/components/search-field';
 import { useFont } from '@/context/FontContext';
 
@@ -7,7 +8,10 @@ export default function Home() {
   const { selectedFont } = useFont();
 
   return (
-    <main className={`${selectedFont.font.className}`}>
+    <main
+      className={`${selectedFont.font.className} w-full min-w-[240px] max-w-[737px] p-6 md:px-10 md:py-14`}
+    >
+      <Header />
       <SearchField />
     </main>
   );
